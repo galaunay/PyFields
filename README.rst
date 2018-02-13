3e0a66f1b0cfe4dc8b21d8cee711e4f71f656ce4
galaunay <gaby.launay@tutanota.com>
2018-02-13 12:42:41 +0000

    First draft of the structure

---
+++ README.rst
@@ -0,0 +1,28 @@
+.. _start-intro:
+====================================
+PyFields - A fields analysis package
+====================================
+..
+   .. image:: https://gitlab.com/gitlab-org/gitlab-ce/badges/master/build.svg
+      :target: https://gitlab.com/gabylaunay/IMTreatment/commits/master
+      :alt: Build status
+   .. image:: https://gitlab.com/gitlab-org/gitlab-ce/badges/master/coverage.svg
+      :target: https://framagit.org/gabylaunay/IMTreatment/pipelines
+      :alt: Overall test coverage
+   .. image:: https://readthedocs.org/projects/imtreatment/badge/?version=latest
+      :target: http://imtreatment.readthedocs.io/en/latest/?badge=latest
+      :alt: Documentation Status
+
+
+This module has been written to carry out analysis on 2D and 3D fields,
+like velocity, concentration, temperature, ... fields.
+It can handle sets of fields, to analyze time evolution of the aforementioned fields.
+This module includes methods for importing field, analysing them, display them and export them in other format.
+
+This project is hosted on Github_.
+
+Full documentation is available on ReadTheDocs_.
+
+.. _Github: https://github.com/galaunay/PyFields
+
+.. _end-intro:
---
+++ setup.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+#!/bin/env python3
+
+# Copyright (C) 2018 Gaby Launay
+
+# Author: Gaby Launay  <gaby.launay@tutanota.com>
+# URL: https://github.com/galaunay/pyfields
+# Version: 0.1
+
+# This file is part of PyFields
+
+# PyFields is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 3
+# of the License, or (at your option) any later version.
+
+# PyFields is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+from setuptools import setup, find_packages
+
+setup(
+    name='PyFields',
+    version='1.0',
+    description='Tools to work with 2D and 3D fields',
+    author='Gaby Launay',
+    author_email='gaby.launay@tutanota.com',
+    license='GPLv3',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering',
+        'License :: OSI Approved :: GPLv3 License',
+        'Programming Language :: Python :: 3.5',
+        'Natural Language :: English',
+        'Operating System :: POSIX :: Linux',
+    ],
+    keywords='fields velocity concentration sets',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests', 'samples']),
+    install_requires=['numpy', 'matplotlib', 'scipy', 'unum'],
+    extras_require={},
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+)
